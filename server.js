42a0629b3d99681ac5300fbf8cc67b4e9d45862b
Vinay jain <jainv823@gmail.com>
2021-02-16 17:41:30 +0530

    mongoDB setup

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const dotenv = require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.5virz.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,{useUnifiedTopology:true,useNewUrlParser:true})
+.then(()=>{console.log("Succesfully connected to mongoDB")});
 
 app.get("/", (req, res) => {
   res.send("Hello world");
