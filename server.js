7d8318e20ffd222eeb9f2094a82620aa43e83512
Vinay jain <jainv823@gmail.com>
2021-02-16 17:13:52 +0530

    Initialized project

---
+++ server.js
@@ -0,0 +1,11 @@
+const dotenv = require("dotenv").config();
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+app.listen(process.env.PORT, () => {console.log(`Server running on port ${process.env.PORT}`);
+});
